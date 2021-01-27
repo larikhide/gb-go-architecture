e5bb04e978727521b6b4326213bd14507f940cf3
Petr Ustyuzhanin <petrakliy@gmail.com>
2021-01-27 16:09:19 +0300

    add TODO for future

--- lesson-4/binarySearch.go
+++ lesson-4/binarySearch.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 )
 
+//TODO: попробовать реализовать через интерфейс и методы для любой сущности, которая имеет длину и отсортирована не привязывясь только к массиву
+
 func binarySearch(arr []int32, reqNum int32) (reqIdx int32, err error) {
 	low := 0
 	high := len(arr) - 1
@@ -30,5 +32,5 @@ func binarySearch(arr []int32, reqNum int32) (reqIdx int32, err error) {
 func main() {
 	arr := []int32{1, 2, 3, 4, 5}
 
-	fmt.Println(binarySearch(arr, 2))
+	fmt.Println(binarySearch(arr, 2)) //TODO: добавить ввод с клавиатуры или через аргументы командной строки
 }
