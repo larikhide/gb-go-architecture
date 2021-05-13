bfb1ab3441258017e5d8af52cb174db85d738138
Peter Ustyuzhanin <petrakliy@gmail.com>
2021-05-13 14:15:23 +0300

    where auth smtp.Client?

--- lesson-2/shop/pkg/email/email.go
+++ lesson-2/shop/pkg/email/email.go
@@ -1,6 +1,8 @@
 package email
 
 import (
+	"fmt"
+	"net"
 	"net/smtp"
 	"shop/models"
 )
@@ -13,6 +15,31 @@ type emailClient struct {
 	cli *smtp.Client
 }
 
+func NewSMTPClient(network, host string) (*emailClient, error) {
+	conn, err := net.Dial(network, host)
+	if err != nil {
+		return nil, err
+	}
+	cli, err := smtp.NewClient(conn, host)
+	if err != nil {
+		return nil, err
+	}
+	return &emailClient{
+		cli: cli,
+	}, nil
+}
+
 func (email *emailClient) SendOrderConfirmation(order *models.Order) error {
-https: //riptutorial.com/ru/go/example/20761/отправка-электронной-почты-с-помощью-smtp-sendmail---
+
+	text := fmt.Sprintf("new order %d\nemail: %s\nphone: %s", order.ID, order.Email, order.Phone)
+
+	//Q: Не уверен, что хорошо делать аунтификацию прямо здесь. На мой взгляд это нужно делать в мэйне.
+	// Как все же правильнее?
+	//auth := smtp.PlainAuth("", "someuser@example.com", "password", "mail.example.com")
+
+	err := smtp.SendMail("mail.example.com:25", auth, "someuser@example.com", []string{order.Email}, []byte(text))
+	if err != nil {
+		return err
+	}
+	return nil
 }
